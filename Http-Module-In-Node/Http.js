d62f62efd7dfc79280a56a54c7adcbc1b04ac7fb
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-18 17:30:20 +0530

    second commit

--- Http-Module-In-Node/Http.js
+++ Http-Module-In-Node/Http.js
@@ -6,8 +6,15 @@ const http = require("http");
 
 const server = http.createServer((req,res)=>{
     if(req.url === "/"){
-        res.write("<h1>We are Success Fully create Server </h1>")
+        res.write("<h1>Home Page </h1>")
 
+    }else if(req.url === "/about"){
+        res.write("<h1>About Page </h1>")
+
+    }else if(req.url === "/contact"){
+        res.write("<h1>Contact Page </h1>")
+    }else{
+        res.write("<h1>Error 404!!</h1>")
     }
     res.end();
 
