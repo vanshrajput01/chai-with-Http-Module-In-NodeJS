17209ce5b1864d8386592f7986eb179d417d323b
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-18 17:26:43 +0530

    success fullly carete first server

--- Http-Module-In-Node/Http.js
+++ Http-Module-In-Node/Http.js
@@ -2,4 +2,20 @@
 
 const http = require("http");
 
-console.log(http);
+// console.log(http);
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.write("<h1>We are Success Fully create Server </h1>")
+
+    }
+    res.end();
+
+});
+
+const port  = 2000;
+
+server.listen(port,()=>{
+    console.log(`The Http Server is run http://localhost:${port}`);
+})
+
